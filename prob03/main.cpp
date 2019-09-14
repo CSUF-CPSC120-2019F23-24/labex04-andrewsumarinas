4084db0d1cd77402333b329cc3519924e4442b86
ALSumarinas <alsumarinas@cs104u-4c.acad.fullerton.edu>
2019-09-14 14:27:25 -0700

    Prob03-AndrewSumarinas

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,23 @@
 // Sales prediction
+
+
+#include <iostream>
+
+int main()
+
+{
+  const double SALES_RATE = 0.18;
+  const int MY_SALES = 2103419277;
+  long nextYear;
+
+  nextYear = (SALES_RATE * MY_SALES) + MY_SALES;
+
+  std::cout << "Last year's sales was " << MY_SALES << std::endl;
+
+  std::cout << "This year's sales prediction will be " << nextYear << std::endl;
+
+  return 0;
+
+
+
+}
